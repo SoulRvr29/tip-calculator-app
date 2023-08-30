1a59ad733c2aeb75e93f3a013e41dcde2928ca2a
Paweł Chudecki <pawelc29@interia.pl>
2023-08-30 10:01:19 +0200

    added footer

---
+++ src/Footer.jsx
@@ -0,0 +1,24 @@
+const Footer = () => {
+  return (
+    <footer className="text-base text-center text-Dark-grayish-cyan p-4 max-sm:text-xs">
+      Challenge by
+      <a
+        className="text-Very-dark-cyan ml-2 hover:text-Strong-cyan hover:underline transition-all duration-300"
+        href="https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX"
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by
+      <a
+        className="text-Very-dark-cyan ml-2 hover:text-Strong-cyan hover:underline transition-all duration-300"
+        href="https://www.frontendmentor.io/profile/SoulRvr29"
+      >
+        Paweł Chudecki
+      </a>
+      .
+    </footer>
+  );
+};
+
+export default Footer;
